901cf8744738d210099f074cad9676d5731f16e9
AlkorMizar <anisa15012002@gmail.com>
2021-11-14 12:50:27 +0300

    ver 0.0

---
+++ TestGenerator/CreateTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGenerator
+{
+    class CreateTest
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
