6a56ff5cc4faf756a609459d340740c048b33eb7
AlkorMizar <anisa15012002@gmail.com>
2021-11-14 22:43:05 +0300

    ver 0.2

--- TestGenerator/Decompose.cs
+++ TestGenerator/Decompose.cs
@@ -1,10 +1,52 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TestGenerator
 {
+    
     class Decompose
     {
+        string GetTypeName(TypeDeclarationSyntax type) {
+            return type.Identifier.ValueText;
+        }
+
+        string GetNamespace(NamespaceDeclarationSyntax namesp) {
+            return namesp.Name.ToString();
+        }
+
+        private string[] GetMethodsNames(TypeDeclarationSyntax type)
+        {
+            var publicMethodsNames = from methodDeclaration in type.DescendantNodes()
+                                                                                 .OfType<MethodDeclarationSyntax>()
+                                     where methodDeclaration.Modifiers.Any(SyntaxKind.PublicKeyword)
+                                     select methodDeclaration.Identifier.ToString();
+
+            return publicMethodsNames.Cast<string>().ToArray();
+        }
+        public IEnumerable<TypeContext> DecomposeType(string programText) { 
+            var list = new List<TypeContext>();
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+            foreach (var member in root.Members)
+            {
+                var namespaceDecl = member as NamespaceDeclarationSyntax;
+                if (namespaceDecl!=null) {
+                    foreach (var memberType in namespaceDecl.Members)
+                    {
+                        if (memberType.Kind()==SyntaxKind.ClassDeclaration || memberType.Kind() == SyntaxKind.StructDeclaration) {
+                            var type = memberType as TypeDeclarationSyntax;
+                            
+                            list.Add(new TypeContext(GetNamespace(namespaceDecl), GetTypeName(type), GetMethodsNames(type)));
+                        }
+                    }
+                }
+            }
+            return list;
+        }
     }
 }
---
+++ TestGenerator/TypeContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGenerator
+{
+    class TypeContext
+    {
+        public string Namespace { get; set; }
+        public string Type { get; set; }
+
+        public string[] Methods { get; set; }
+
+        public TypeContext(string _namespace, string _type, string[] _methods) {
+            Namespace = _namespace;
+            Type = _type;
+            Methods = _methods;
+        }
+
+        public TypeContext(string _namespace, string _type):this(_namespace,_type,null) { 
+        }
+        public TypeContext(string _namespace) : this(_namespace, null, null)
+        {
+        }
+    }
+}
